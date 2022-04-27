ba1d5bd2e51a2f9601bb63b17a430d8e1a23c4ff
Kruize Derek Christensen <christensen.kruize@gmail.com>
2022-04-27 16:00:26 -0600

    Create arrow.js

---
+++ arrow.js
@@ -0,0 +1,10 @@
+ penUp(); 
+onEvent("redbtn", "click", function(){
+moveForward(25);
+});
+onEvent("turnLeft", "click", function(){
+turnLeft(90);
+});
+onEvent("Right", "click", function(){
+turnRight(90);
+});
